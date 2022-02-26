2584f9439093b56a34faa3161b6a7883c0aca462
svyatoslav <karamushkoss@gmail.com>
2022-02-27 01:28:40 +0300

    Произведена реструктуиризация метода updateArray

--- src/Serializer/EntityObjectSerializer.php
+++ src/Serializer/EntityObjectSerializer.php
@@ -32,40 +32,53 @@ class EntityObjectSerializer
         $reflectionProperties = $reflectionClass->getProperties();
 
         foreach ($reflectionProperties as $property) {
-            $propertyType = ReflectionHelper::getPropertyType(new $objectName(),  $property->getName());
+            if (isset($params[$property->getName()])) {
+                $propertyType = ReflectionHelper::getPropertyType(new $objectName(), $property->getName());
 
-            if ($propertyType &&
-                class_exists($propertyType) &&
-                !$this->em->getMetadataFactory()->isTransient($propertyType) &&
-                isset($params[$property->getName()]) &&
-                is_int($params[$property->getName()])
-            ) {
-                $params[$property->getName()] = $this->em->getRepository($propertyType)->find($params[$property->getName()]);
-            }
+                $params[$property->getName()] = $this->updateToEntity($params[$property->getName()], $propertyType);
 
-            if ($propertyType &&
-                class_exists($propertyType) &&
-                $propertyType === ArrayCollection::class &&
-                isset($params[$property->getName()]) &&
-                is_array($params[$property->getName()])
-            ) {
-                $defaultValue = $params[$property->getName()];
-                $params[$property->getName()] = new ArrayCollection();
-                foreach ($defaultValue as $id) {
-                    if (!is_int($id)) {
-                        $params[$property->getName()] = $defaultValue;
-                        break;
-                    }
-
-                    $collectionType = ReflectionHelper::getArrayCollectionPropertyTarget(new $objectName(),  $property->getName());
-
-                    $arrayCollObject = $this->em->getRepository($collectionType)->find($id);
-
-                    $params[$property->getName()]->add($arrayCollObject);
-                }
+                $params[$property->getName()] = $this->updateToCollection($params[$property->getName()], $property, $propertyType);
             }
         }
 
         return $params;
+    }
+
+    private function updateToEntity($value, string $propertyType)
+    {
+        if ($propertyType &&
+            class_exists($propertyType) &&
+            !$this->em->getMetadataFactory()->isTransient($propertyType) &&
+            is_int($value)
+        ) {
+            $value = $this->em->getRepository($propertyType)->find($value);
+        }
+
+        return $value;
+    }
+
+    private function updateToCollection($value, \ReflectionProperty $property, string $propertyType)
+    {
+        if ($propertyType &&
+            class_exists($propertyType) &&
+            $propertyType === ArrayCollection::class &&
+            is_array($value)
+        ) {
+            $defaultValue = $value;
+            $value = new ArrayCollection();
+            foreach ($defaultValue as $id) {
+                if (!is_int($id)) {
+                    $value = $defaultValue;
+                    break;
+                }
+
+                $className = $property->getDeclaringClass()->getName();
+                $collectionType = ReflectionHelper::getArrayCollectionPropertyTarget(new $className(),  $property->getName());
+                $arrayCollObject = $this->em->getRepository($collectionType)->find($id);
+                $value->add($arrayCollObject);
+            }
+        }
+
+        return $value;
     }
 }
